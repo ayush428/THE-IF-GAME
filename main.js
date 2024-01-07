c38bcdf1b947f343d04128cf2ee524eccc35eba7
ayush428 <127584051+ayush428@users.noreply.github.com>
2024-01-07 23:09:09 +0530

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 var arr = ['Racism Ends',
           'Germany restores to its former glory',
            'Greenland is filled with poisonous spiders',
-           'The world reunites with Einstein
+           'The world reunites with Einstein'
           ]
 function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
@@ -10,8 +10,8 @@ function getRandomInt(min, max) {
 const len = arr.length
 document.addEventListener("DOMContentLoaded", function() {
     // Access the button by its name
-    var ifbButton = document.getElementsByName("IFb")[0];
-    var thenbButton = document.getElementsByName("THENb")[0];
+    var ifbButton = document.getElementsById("IFb");
+    var thenbButton = document.getElementsById("THENb");
 
     // Do something with the button
     ifbButton.addEventListener("click", function() {
