c8720afe77a3f3063bcf82e59d10e015e829548d
Beastojenisto <131438414+Beastojenisto@users.noreply.github.com>
2024-01-08 12:40:50 +0530

    Update main.js

--- main.js
+++ main.js
@@ -3,19 +3,19 @@ var arr = ['Racism Ends',
           'Germany restores to its former glory',
            'Greenland is filled with poisonous spiders',
            'The world reunites with Einstein'
-          ]
+          ];
 function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-const len = arr.length
+};
+const len = arr.length;
 function onClickthend(){
-          var thendiv = document.getElementByID('thend')
-          thendiv.innerHTML = arr[getRandomInt(0,len)]
-}
+          var thendiv = document.getElementByID('thend');
+          thendiv.innerHTML = arr[getRandomInt(0,len)];
+};
 function onClickifd(){
-          var ifdiv = document.getElementByID('ifd')
-          ifdiv.innerHTML = arr[getRandomInt(0,len)]
-}
+          var ifdiv = document.getElementByID('ifd');
+          ifdiv.innerHTML = arr[getRandomInt(0,len)];
+};
 
 // document.addEventListener("DOMContentLoaded", function() {
 //     // Access the button by its name
